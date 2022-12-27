34adaab511a130069413ec0f91cbb3b9a96c8a8f
Erasmo Jr <erasmo.junior@gmail.com>
2022-12-27 01:02:02 -0300

    :construction: FEAT: Logic delete expense by Id

---
+++ src/test/java/io/red/spent/services/DeleteExpenseServiceTest.java
@@ -0,0 +1,11 @@
+package io.red.spent.services;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+class DeleteExpenseServiceTest {
+
+    @Test
+    void deleteLogicBy() {
+    }
+}
