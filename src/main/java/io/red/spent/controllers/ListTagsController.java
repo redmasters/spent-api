56da4331e37ecd76ee113eb45d63f9e1e83d7161
Erasmo Jr <erasmo.cjunior@gmail.com>
2023-01-03 16:10:14 -0300

    :bug: FIX: (ListTagsController) adding cross

--- src/main/java/io/red/spent/controllers/ListTagsController.java
+++ src/main/java/io/red/spent/controllers/ListTagsController.java
@@ -1,12 +1,14 @@
 package io.red.spent.controllers;
 
 import io.red.spent.services.ListTagService;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
+@CrossOrigin(origins = "*")
 @RestController
 @RequestMapping("/v1/expense")
 public class ListTagsController {
